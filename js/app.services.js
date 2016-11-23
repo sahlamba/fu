c100549aad41fa3851750d3647f0a5d779754a72
Sahil Lamba <sahil.lamba95@gmail.com>
2016-11-23 18:46:42 +0530

    Move faker offline

--- js/app.services.js
+++ js/app.services.js
@@ -9,29 +9,21 @@ angular.module('fuck.service')
       return {
         createUser: function (userName) {
           var defer = $q.defer();
-          var superName = 'random';
-          $http.get("http://faker.hook.io/?property=internet.domainWord")
-            .success(function (data) {
-              superName = data.replace(/"/g, ''); // Remove quotes
-              superName = superName.toLowerCase().slice(0, -1); // Trim + remove newline char
-              var randomInt = Math.floor(Math.random() * (99 - 0 + 1)) + 0; // 0 <= int <= 99
-              var userURL = 'users/' + superName + '-' + randomInt;
-              userName = userName.toLowerCase().trim().replace(/\b\w/g, function (l) {
-                return l.toUpperCase();
-              }); // Capitalize string
-              db.ref(userURL).set({
-                'name': userName
-              });
-              defer.resolve({
-                'id': superName + '-' + randomInt,
-                'status': true
-              });
-            }).error(function (error) {
-                defer.resolve({
-                  'status': false
-                });
-                console.error(error);
-            });
+          var superName = faker.internet.domainWord();
+          superName = superName.replace(/"/g, ''); // Remove quotes
+          superName = superName.toLowerCase().slice(0, -1); // Trim + remove newline char
+          var randomInt = Math.floor(Math.random() * (99 - 0 + 1)) + 0; // 0 <= int <= 99
+          var userURL = 'users/' + superName + '-' + randomInt;
+          userName = userName.toLowerCase().trim().replace(/\b\w/g, function (l) {
+            return l.toUpperCase();
+          }); // Capitalize string
+          db.ref(userURL).set({
+            'name': userName
+          });
+          defer.resolve({
+            'id': superName + '-' + randomInt,
+            'status': true
+          });
           return defer.promise;
         },
         getUser: function (keyStoneString) {
